1a271c919628545be1ae6ca532300bab90b6a142
Weizhen Wang <wangweizhen@pingcap.com>
2021-07-02 19:17:45 +0800

    update feature

--- django_tidb/__init__.py
+++ django_tidb/__init__.py
@@ -2,4 +2,8 @@
 # wrong version of Django is installed.
 from .utils import check_django_compatability
 
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution("django-tidb").version
+
 check_django_compatability()
