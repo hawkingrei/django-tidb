b64c827d5ec006afe1d2b523d29e91345f93f29c
Weizhen Wang <wangweizhen@pingcap.com>
2021-07-02 18:37:20 +0800

    update feature

--- django_tidb/client.py
+++ django_tidb/client.py
@@ -2,7 +2,7 @@ from django.db.backends.base.client import BaseDatabaseClient
 
 
 class DatabaseClient(BaseDatabaseClient):
-    executable_name = 'mysql'
+    executable_name = 'tidb'
 
     @classmethod
     def settings_to_cmd_args_env(cls, settings_dict, parameters):
