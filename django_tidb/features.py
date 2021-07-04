f1d2d6edd033d7cb209e925a3c3e64530e218b60
Weizhen Wang <wangweizhen@pingcap.com>
2021-07-04 09:42:57 +0800

    update feature for tidb

--- django_tidb/features.py
+++ django_tidb/features.py
@@ -118,7 +118,7 @@ class DatabaseFeatures(MysqlDatabaseFeatures):
 
     @cached_property
     def supported_explain_formats(self):
-        return {'TEXT', 'TRADITIONAL'}
+        return {'ROW'}
 
     @cached_property
     def ignores_table_name_case(self):
--- django_tidb/operations.py
+++ django_tidb/operations.py
@@ -10,20 +10,15 @@ from django.db.backends.mysql.operations import (
 
 class DatabaseOperations(MysqlDatabaseOperations):
     def explain_query_prefix(self, format=None, **options):
-        # Alias MySQL's TRADITIONAL to TEXT for consistency with other backends.
-        if format and format.upper() == 'TEXT':
-            format = 'TRADITIONAL'
-        elif not format and 'TREE' in self.connection.features.supported_explain_formats:
-            # Use TREE by default (if supported) as it's more informative.
-            format = 'TREE'
         analyze = options.pop('analyze', False)
         prefix = super().explain_query_prefix(format, **options)
         if analyze and self.connection.features.supports_explain_analyze:
             # MariaDB uses ANALYZE instead of EXPLAIN ANALYZE.
             prefix += ' ANALYZE'
+        #TODO support format
         if format and not analyze:
             # Only MariaDB supports the analyze option with formats.
-            prefix += ' FORMAT=%s' % format
+            prefix += ' FORMAT=\'%s\'' % format
         return prefix
 
     def regex_lookup(self, lookup_type):
