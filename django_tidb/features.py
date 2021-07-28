16c41c3a4278341be1f4b86134de1380a156c349
Weizhen Wang <wangweizhen@pingcap.com>
2021-07-28 11:32:02 +0800

    feat: enable expression_indexes when tidb version > 5.1

--- django_tidb/features.py
+++ django_tidb/features.py
@@ -430,4 +430,4 @@ class DatabaseFeatures(MysqlDatabaseFeatures):
 
     @cached_property
     def supports_expression_indexes(self):
-        return True
+        return self.connection.tidb_version >= (5, 1, )
