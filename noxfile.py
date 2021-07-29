6e01bed546ef9e51e24cd26df31afbf118756f63
Weizhen Wang <wangweizhen@pingcap.com>
2021-07-29 17:24:16 +0800

    change copyright

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright 2020 Google LLC
-#
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file or at
-# https://developers.google.com/open-source/licenses/bsd
-
-
 from __future__ import absolute_import
 
 import os
